69b0787c3d24cd850b0e632440e5ef2a1845ca94
Nobel76 <nobelpantra@gmail.com>
2023-03-28 22:29:24 +0600

    added commit

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = ({cart}) => {
+    let totalPrice= 0;
+    let totalShipping = 0;
+    for(const product of cart){
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+    const tax = totalPrice * 7/100;
+    const grandTotal = totalPrice + totalShipping + tax;
+    return (
+        <div className='cart'>
+              <h4>Order Summary</h4>
+              <p>Selected Items: {cart.length}</p>
+              <p>total price:${totalPrice}</p>
+              <p>Shipping:${totalShipping} </p>
+              <p>Tax:${tax} </p>
+              <h6>Grand total:${grandTotal}</h6>
+        </div>
+    );
+};
+
+export default Cart;
